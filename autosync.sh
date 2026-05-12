05ae4bbee6a26c160813d81ac8024eed6bc4aedd
hemanthkunta <hemanthkuntanani999@gmail.com>
2026-05-12 19:07:35 +0530

    Auto sync Tue May 12 19:07:35 IST 2026

--- autosync.sh
+++ autosync.sh
@@ -2,10 +2,18 @@
 
 while true
 do
+    echo "Checking for remote updates..."
+
+    git pull --rebase origin master
+
     if [[ -n $(git status --porcelain) ]]; then
+        echo "Local changes detected..."
+
         git add .
-        git commit -m "Auto sync $(date)"
-        git push
+
+        git commit -m "Auto sync $(date)" || true
+
+        git push origin master
     fi
 
     sleep 10
